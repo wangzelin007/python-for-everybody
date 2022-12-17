869d086a135c883d3f20e057adece56a153fa062
ZelinWang <zelinwang@microsoft.com>
2022-12-17 16:00:52 +0800

    update

---
+++ 630_Unit_Tests/test2.py
@@ -0,0 +1,9 @@
+import unittest
+
+class WidgetTestCase(unittest.TestCase):
+    def test_default_widget_size(self):
+        if False:
+          self.assertIn('d', 'abc')
+
+if __name__ == "__main__":
+    unittest.main()
